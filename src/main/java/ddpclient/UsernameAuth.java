b6c413e5262fd85f9ecb1a64ff617b91011cb0e8
Yasushi Sakai <yasushis@mit.edu>
2015-12-29 16:58:39 -0500

    added modification statement for licence

--- src/main/java/ddpclient/UsernameAuth.java
+++ src/main/java/ddpclient/UsernameAuth.java
@@ -12,6 +12,14 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
+*
+* Modifications Copyright (C) 2015 Yasushi Sakai
+* Modifications based upon the usage inside a Processing sketch
+* - changed package name
+* - added member PApplet variable
+* - deleted slf4j Logging, and use println for inspection
+* - deleted JUnit test
+* - added functions dispose
 */
 
 package ddpclient;
